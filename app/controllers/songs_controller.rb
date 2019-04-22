1f8aa4c030afcde4c48f4a9d47d6e2fdc3059886
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-22 06:20:34 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(params[:song])
+    @song = Song.new(params)
     @song.save
     redirect_to song_path(@song)
   end
