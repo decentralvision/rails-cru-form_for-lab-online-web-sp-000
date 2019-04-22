8cd97b4cec2e821addb88de74bc68fbcb532a2b5
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-22 04:51:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  get 'songs/new'
+
+  get 'songs/edit'
+
+  get 'songs/show'
+
+  get 'genres/new'
+
+  get 'genres/edit'
+
+  get 'genres/show'
+
+  get 'artists/new'
+
+  get 'artists/edit'
+
+  get 'artists/show'
+
+  resources :songs
+  resources :genres
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
