8ca3491551f3587501e64015d2e0510f80aa225d
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-04-22 06:34:31 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -15,7 +15,7 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    @artist = Artist.new(params[:artist])
+    @artist = Artist.new(params)
     @artist.save
     redirect_to artist_path(@artist)
   end
